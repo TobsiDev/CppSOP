a030b41583edd3eadd16394ed0cd69c6ec0fdefe
Tobsi <tsh40884137@gmail.com>
2021-08-15 23:21:02 +0200

    Init.

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include <SDL2/SDL.h>
+//#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
+
+//using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+    // returns zero on success else non-zero
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        printf("error initializing SDL: %s\n", SDL_GetError());
+    }
+    SDL_Window *win = SDL_CreateWindow("GAME", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                       1000, 1000, SDL_WINDOW_BORDERLESS);
+    while (1)
+        ;
+
+    return 0;
+}
